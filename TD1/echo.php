5ff4eebccd03a292278a0eeff1ba89e689ee4789
David Binaud <david.binaud@gmail.com>
2019-09-05 14:53:22 +0200

    Sur la branche master
     Validation initiale
    
     Modifications qui seront validées :
    	nouveau fichier : TD1/echo.php
    	nouveau fichier : TD1/page1.html

---
+++ TD1/echo.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title> My first PHP </title>
+    </head>
+   
+    <body>
+        Here is the output of the PHP script : 
+        <?php
+          // This is a one line PHP comment
+          /* This other way to write comments can 
+          go on many lines */
+           
+          // Let's assign the string "hello" to the variable 'texte'
+          // Variable names start with $ in PHP
+          $texte = "hello world !";
+
+          // Let's write the value of variable '$texte'
+          echo $texte;
+        ?>
+    </body>
+</html> 
