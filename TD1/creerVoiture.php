8fd52b7412ae3c883dcbaa943ecab3d8e30a2023
David Binaud <david.binaud@gmail.com>
2019-09-11 08:39:34 +0200

    Fin exo 9, recuperation donnees

--- TD1/creerVoiture.php
+++ TD1/creerVoiture.php
@@ -3,12 +3,21 @@
 
 require_once('./voiture.php');
 
-$voiture1 = new Voiture($_GET(marque),$_GET(couleur),$_GET(immatriculation));
+/*	
+	if(!empty($_GET)){
+		$voiture1 = new Voiture($_GET['marque'],$_GET['couleur'],$_GET['immatriculation']);
 
-$voiture1->afficher();
+		$voiture1->afficher();
+	}
+	else{ echo "Aucune information sur la voiture transmise";}
+*/
+//Changer la méthode de recupération dans formulairevoiture.html en post ou get
+	if(!empty($_POST)){
+		$voiture1 = new Voiture($_POST['marque'],$_POST['couleur'],$_POST['immatriculation']);
 
-
-
+		$voiture1->afficher();
+	}
+	else{ echo "Aucune information sur la voiture transmise";}
 
 
 ?>
