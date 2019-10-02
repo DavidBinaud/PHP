1c5e53efaa1b18c565c858b06b589d6918a607af
David Binaud <david.binaud@gmail.com>
2019-10-02 12:12:57 +0200

    correction created.php FIN!

--- TD5/view/voiture/created.php
+++ TD5/view/voiture/created.php
@@ -1,4 +1,4 @@
 <?php
 	echo "<p>La voiture a bien été créée !</p>";
-	require File::build_path(array("view","voiture","list.php"));
+	require File::build_path(array("view",$controller,"list.php"));
 ?>
