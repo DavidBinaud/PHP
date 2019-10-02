598b3f1c04aab5d177e80b8f7e84c963651eba36
David Binaud <david.binaud@gmail.com>
2019-10-02 09:59:24 +0200

    TD5 fin exo 3 portabilité site

--- TD5/index.php
+++ TD5/index.php
@@ -1,5 +1,7 @@
 <?php
-	require_once "./lib/File.php";
+	$DS = DIRECTORY_SEPARATOR;
+	$ROOT_FOLDER = __DIR__;
+	require_once "{$ROOT_FOLDER}{$DS}lib{$DS}File.php";
 	require_once (File::build_path(array("controller","routeur.php")));
 
 ?>
--- TD5/lib/File.php
+++ TD5/lib/File.php
@@ -3,8 +3,9 @@
 		public static function build_path($path_array) {
 		    // $ROOT_FOLDER (sans slash à la fin) vaut
 		    // "/home/ann2/votre_login/public_html/TD5" à l'IUT 
-		    $ROOT_FOLDER = "/home/ann2/binaudd/public_html/PHP/TD5";
-		    return $ROOT_FOLDER. '/' . join('/', $path_array);
+		    $DS = DIRECTORY_SEPARATOR;
+			$ROOT_FOLDER = __DIR__ . $DS . "..";
+		    return $ROOT_FOLDER . $DS . join($DS, $path_array);
 		}
 
 
