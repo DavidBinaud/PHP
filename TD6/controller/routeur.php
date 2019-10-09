91bb31c0b401910896518552186a3c38bb15717a
David Binaud <david.binaud@gmail.com>
2019-10-09 09:27:42 +0200

    Fin Exo 1 - action par défaut

--- TD6/controller/routeur.php
+++ TD6/controller/routeur.php
@@ -2,7 +2,12 @@
 //require_once './ControllerVoiture.php';
 require_once (File::build_path(array("controller","ControllerVoiture.php")));
 // On recupère l'action passée dans l'URL
-$action = $_GET['action'];
-// Appel de la méthode statique $action de ControllerVoiture
-ControllerVoiture::$action(); 
+if(isset($_GET['action'])){
+	$action = $_GET['action'];
+	// Appel de la méthode statique $action de ControllerVoiture
+	ControllerVoiture::$action(); 
+}
+else{
+	ControllerVoiture::readAll();
+}
 ?>
