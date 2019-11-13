51d111a8bb6b59c8a649edf4990b197b464a8fa1
David Binaud <david.binaud@etu.umontpellier.fr>
2019-11-13 10:43:54 +0100

    Impossibilité pour les autres de voir mon site

--- TD8/index.php
+++ TD8/index.php
@@ -1,35 +1,39 @@
 <?php
 	session_set_cookie_params(30*60);
 	session_start();
+	if (isset($_COOKIE,$_COOKIE['PHPSESSID']) && $_COOKIE['PHPSESSID'] == 'binaudd') {
+		
+		if (isset($_SESSION['LAST_ACTIVITY']) && time() - $_SESSION['LAST_ACTIVITY'] > (10*60)) {
+	     	// if last request was more than 30 minutes ago
+	     	unset($_SESSION['panier']);     // unset $_SESSION variable for the run-time
+	 	}
 
-	if (time() - $_SESSION['LAST_ACTIVITY'] > (10*60)) {
-     	// if last request was more than 30 minutes ago
-     	unset($_SESSION['panier']);     // unset $_SESSION variable for the run-time
- 	}
+		if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > (30*60))) {
 
-	if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > (30*60))) {
-
-     	// if last request was more than 30 minutes ago
-     	session_unset();     // unset $_SESSION variable for the run-time 
-     	session_destroy();   // destroy session data in storage
- 	} else {
-     	$_SESSION['LAST_ACTIVITY'] = time(); // update last activity time stamp
- 	}
+	     	// if last request was more than 30 minutes ago
+	     	session_unset();     // unset $_SESSION variable for the run-time 
+	     	session_destroy();   // destroy session data in storage
+	 	} else {
+	     	$_SESSION['LAST_ACTIVITY'] = time(); // update last activity time stamp
+	 	}
 
 
 
 
- 	$_SESSION['prixpanier'] = 0;
+	 	$_SESSION['prixpanier'] = 0;
 
- 	if (isset($_SESSION['panier'])) {
- 		foreach ($_SESSION['panier'] as $key => $produit) {
- 			$_SESSION['prixpanier'] += $produit['quantité'];
- 		}
- 	}
+	 	if (isset($_SESSION['panier'])) {
+	 		foreach ($_SESSION['panier'] as $key => $produit) {
+	 			$_SESSION['prixpanier'] += $produit['quantité'];
+	 		}
+	 	}
 
-	$DS = DIRECTORY_SEPARATOR;
-	$ROOT_FOLDER = __DIR__;
-	require_once "{$ROOT_FOLDER}{$DS}lib{$DS}File.php";
-	require_once (File::build_path(array("controller","routeur.php")));
-
+		$DS = DIRECTORY_SEPARATOR;
+		$ROOT_FOLDER = __DIR__;
+		require_once "{$ROOT_FOLDER}{$DS}lib{$DS}File.php";
+		require_once (File::build_path(array("controller","routeur.php")));
+	}
+	else{
+		echo 'NIQUE TA MERE';
+	}
 ?>
