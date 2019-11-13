f338594e49696c3f043bb9c72df12daeb64b248c
David Binaud <david.binaud@etu.umontpellier.fr>
2019-11-13 19:12:00 +0100

    TD8: Fin Exo 4

--- TD8/lib/Security.php
+++ TD8/lib/Security.php
@@ -2,9 +2,17 @@
 
 	class Security{
 
+		private static $seed = 'GEMCVO4I4A';
+
 		static function chiffrer($texte_en_clair) {
-			$texte_chiffre = hash('sha256', $texte_en_clair);
+			$texte_chiffre = hash('sha256', self::$seed . $texte_en_clair);
 			return $texte_chiffre;
+		}
+
+
+
+		static public function getSeed() {
+		   return self::$seed;
 		}
 	}
 ?>
